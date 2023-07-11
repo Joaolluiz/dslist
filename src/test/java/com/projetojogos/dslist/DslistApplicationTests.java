c35c93d8bc294f27d676bef87cadf7f0e7c3de86
Joaolluiz <joao.fdk8@gmail.com>
2023-07-11 14:30:02 -0300

    Project Created

---
+++ src/test/java/com/projetojogos/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetojogos.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
